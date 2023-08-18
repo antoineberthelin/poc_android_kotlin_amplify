a7bd39f65efb51e73ae3459bc0f9f45862d66b4f
Antoine Berthelin <antoine.berthelin@appsonic.fr>
2023-08-18 10:02:44 +0200

    POC BUG on DataStoreConfiguration syncExpression with index

---
+++ app/src/main/java/com/amplifyframework/datastore/generated/model/User.java
@@ -0,0 +1,225 @@
+package com.amplifyframework.datastore.generated.model;
+
+import com.amplifyframework.core.model.temporal.Temporal;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.Objects;
+
+import androidx.core.util.ObjectsCompat;
+
+import com.amplifyframework.core.model.Model;
+import com.amplifyframework.core.model.annotations.Index;
+import com.amplifyframework.core.model.annotations.ModelConfig;
+import com.amplifyframework.core.model.annotations.ModelField;
+import com.amplifyframework.core.model.query.predicate.QueryField;
+
+import static com.amplifyframework.core.model.query.predicate.QueryField.field;
+
+/** This is an auto generated class representing the User type in your schema. */
+@SuppressWarnings("all")
+@ModelConfig(pluralName = "Users", type = Model.Type.USER, version = 1)
+@Index(name = "byLastName", fields = {"lastName","createdAt"})
+public final class User implements Model {
+  public static final QueryField ID = field("User", "id");
+  public static final QueryField FIRST_NAME = field("User", "firstName");
+  public static final QueryField LAST_NAME = field("User", "lastName");
+  public static final QueryField CREATED_AT = field("User", "createdAt");
+  private final @ModelField(targetType="ID", isRequired = true) String id;
+  private final @ModelField(targetType="String", isRequired = true) String firstName;
+  private final @ModelField(targetType="String", isRequired = true) String lastName;
+  private final @ModelField(targetType="AWSDateTime", isRequired = true) Temporal.DateTime createdAt;
+  private @ModelField(targetType="AWSDateTime", isReadOnly = true) Temporal.DateTime updatedAt;
+  public String resolveIdentifier() {
+    return id;
+  }
+  
+  public String getId() {
+      return id;
+  }
+  
+  public String getFirstName() {
+      return firstName;
+  }
+  
+  public String getLastName() {
+      return lastName;
+  }
+  
+  public Temporal.DateTime getCreatedAt() {
+      return createdAt;
+  }
+  
+  public Temporal.DateTime getUpdatedAt() {
+      return updatedAt;
+  }
+  
+  private User(String id, String firstName, String lastName, Temporal.DateTime createdAt) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.createdAt = createdAt;
+  }
+  
+  @Override
+   public boolean equals(Object obj) {
+      if (this == obj) {
+        return true;
+      } else if(obj == null || getClass() != obj.getClass()) {
+        return false;
+      } else {
+      User user = (User) obj;
+      return ObjectsCompat.equals(getId(), user.getId()) &&
+              ObjectsCompat.equals(getFirstName(), user.getFirstName()) &&
+              ObjectsCompat.equals(getLastName(), user.getLastName()) &&
+              ObjectsCompat.equals(getCreatedAt(), user.getCreatedAt()) &&
+              ObjectsCompat.equals(getUpdatedAt(), user.getUpdatedAt());
+      }
+  }
+  
+  @Override
+   public int hashCode() {
+    return new StringBuilder()
+      .append(getId())
+      .append(getFirstName())
+      .append(getLastName())
+      .append(getCreatedAt())
+      .append(getUpdatedAt())
+      .toString()
+      .hashCode();
+  }
+  
+  @Override
+   public String toString() {
+    return new StringBuilder()
+      .append("User {")
+      .append("id=" + String.valueOf(getId()) + ", ")
+      .append("firstName=" + String.valueOf(getFirstName()) + ", ")
+      .append("lastName=" + String.valueOf(getLastName()) + ", ")
+      .append("createdAt=" + String.valueOf(getCreatedAt()) + ", ")
+      .append("updatedAt=" + String.valueOf(getUpdatedAt()))
+      .append("}")
+      .toString();
+  }
+  
+  public static FirstNameStep builder() {
+      return new Builder();
+  }
+  
+  /**
+   * WARNING: This method should not be used to build an instance of this object for a CREATE mutation.
+   * This is a convenience method to return an instance of the object with only its ID populated
+   * to be used in the context of a parameter in a delete mutation or referencing a foreign key
+   * in a relationship.
+   * @param id the id of the existing item this instance will represent
+   * @return an instance of this model with only ID populated
+   */
+  public static User justId(String id) {
+    return new User(
+      id,
+      null,
+      null,
+      null
+    );
+  }
+  
+  public CopyOfBuilder copyOfBuilder() {
+    return new CopyOfBuilder(id,
+      firstName,
+      lastName,
+      createdAt);
+  }
+  public interface FirstNameStep {
+    LastNameStep firstName(String firstName);
+  }
+  
+
+  public interface LastNameStep {
+    CreatedAtStep lastName(String lastName);
+  }
+  
+
+  public interface CreatedAtStep {
+    BuildStep createdAt(Temporal.DateTime createdAt);
+  }
+  
+
+  public interface BuildStep {
+    User build();
+    BuildStep id(String id);
+  }
+  
+
+  public static class Builder implements FirstNameStep, LastNameStep, CreatedAtStep, BuildStep {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private Temporal.DateTime createdAt;
+    @Override
+     public User build() {
+        String id = this.id != null ? this.id : UUID.randomUUID().toString();
+        
+        return new User(
+          id,
+          firstName,
+          lastName,
+          createdAt);
+    }
+    
+    @Override
+     public LastNameStep firstName(String firstName) {
+        Objects.requireNonNull(firstName);
+        this.firstName = firstName;
+        return this;
+    }
+    
+    @Override
+     public CreatedAtStep lastName(String lastName) {
+        Objects.requireNonNull(lastName);
+        this.lastName = lastName;
+        return this;
+    }
+    
+    @Override
+     public BuildStep createdAt(Temporal.DateTime createdAt) {
+        Objects.requireNonNull(createdAt);
+        this.createdAt = createdAt;
+        return this;
+    }
+    
+    /**
+     * @param id id
+     * @return Current Builder instance, for fluent method chaining
+     */
+    public BuildStep id(String id) {
+        this.id = id;
+        return this;
+    }
+  }
+  
+
+  public final class CopyOfBuilder extends Builder {
+    private CopyOfBuilder(String id, String firstName, String lastName, Temporal.DateTime createdAt) {
+      super.id(id);
+      super.firstName(firstName)
+        .lastName(lastName)
+        .createdAt(createdAt);
+    }
+    
+    @Override
+     public CopyOfBuilder firstName(String firstName) {
+      return (CopyOfBuilder) super.firstName(firstName);
+    }
+    
+    @Override
+     public CopyOfBuilder lastName(String lastName) {
+      return (CopyOfBuilder) super.lastName(lastName);
+    }
+    
+    @Override
+     public CopyOfBuilder createdAt(Temporal.DateTime createdAt) {
+      return (CopyOfBuilder) super.createdAt(createdAt);
+    }
+  }
+  
+}
--- app/src/main/java/com/appsonic/poc/amplify/MainActivity.kt
+++ app/src/main/java/com/appsonic/poc/amplify/MainActivity.kt
@@ -4,39 +4,28 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenuBox
-import androidx.compose.material3.ExposedDropdownMenuDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.window.PopupProperties
 import androidx.lifecycle.coroutineScope
-import androidx.lifecycle.viewModelScope
 import com.amplifyframework.AmplifyException
 import com.amplifyframework.api.aws.AWSApiPlugin
+import com.amplifyframework.api.aws.ApiAuthProviders
+import com.amplifyframework.api.aws.AuthModeStrategyType
+import com.amplifyframework.api.aws.sigv4.DefaultCognitoUserPoolsAuthProvider
+import com.amplifyframework.api.graphql.model.ModelQuery
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
+import com.amplifyframework.core.model.query.Where
 import com.amplifyframework.datastore.AWSDataStorePlugin
+import com.amplifyframework.datastore.DataStoreConfiguration
 import com.amplifyframework.datastore.DataStoreException
-import com.amplifyframework.datastore.generated.model.Todo
+import com.amplifyframework.datastore.generated.model.User
 import com.amplifyframework.kotlin.core.Amplify
 import com.appsonic.poc.amplify.ui.theme.POC_AMPLIFYTheme
 import kotlinx.coroutines.Dispatchers
@@ -45,9 +34,7 @@ import kotlinx.coroutines.launch
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        //initAmplify(this)
-
-        //addRandomTodo()
+        initAmplify(this)
 
         super.onCreate(savedInstanceState)
         setContent {
@@ -59,222 +46,23 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Column {
 
-                    Greeting("Android")
+                        Greeting("Android")
 
-//                    EditableExposedDropdownMenuSample()
-
-  //                      ExposedDropdownMenuSample()
-
-    //                    Sample2()
-
-                        docs()
+                        Button(onClick = {
+                            refreshdatastore()
+                        }) {
+                            Text(text = "Refresh data store")
+                        }
                     }
                 }
             }
         }
     }
 
-    @OptIn(ExperimentalMaterial3Api::class)
-    @Composable
-    fun ExposedDropdownMenuSample() {
-        val options = listOf("Option 1", "Option 2", "Option 3", "Option 4", "Option 5")
-        var expanded by remember { mutableStateOf(false) }
-        var selectedOptionText by remember { mutableStateOf(options[0]) }
-        // We want to react on tap/press on TextField to show menu
-        ExposedDropdownMenuBox(
-            expanded = expanded,
-            onExpandedChange = {
-                expanded = !expanded
-            }
-        ) {
-            TextField(
-                readOnly = true,
-                value = selectedOptionText,
-                onValueChange = { },
-                label = { Text("Label") },
-                trailingIcon = {
-                    ExposedDropdownMenuDefaults.TrailingIcon(
-                        expanded = expanded
-                    )
-                },
-                colors = ExposedDropdownMenuDefaults.textFieldColors()
-            )
-            ExposedDropdownMenu(
-                expanded = expanded,
-                onDismissRequest = {
-                    expanded = false
-                }
-            ) {
-                options.forEach { selectionOption ->
-                    DropdownMenuItem(
-                        onClick = {
-                            selectedOptionText = selectionOption
-                            expanded = false
-                        },
-                        text = {Text(text = selectionOption)}
-                    )
-                }
-            }
-        }
-    }
-
-    @OptIn(ExperimentalMaterial3Api::class)
-    @Composable
-    fun EditableExposedDropdownMenuSample() {
-        val options = listOf("Option 1", "Option 2", "Option 3", "Option 4", "Option 5")
-        var expanded by remember { mutableStateOf(false) }
-        var selectedOptionText by remember { mutableStateOf("") }
-        ExposedDropdownMenuBox(
-            expanded = expanded,
-            onExpandedChange = {
-                expanded = !expanded
-            }
-        ) {
-            TextField(
-                value = selectedOptionText,
-                onValueChange = { selectedOptionText = it },
-                label = { Text("Label") },
-                trailingIcon = {
-                    ExposedDropdownMenuDefaults.TrailingIcon(
-                        expanded = expanded
-                    )
-                },
-                colors = ExposedDropdownMenuDefaults.textFieldColors()
-            )
-            // filter options based on text field value
-            val filteringOptions =
-                options.filter { it.contains(selectedOptionText, ignoreCase = true) }
-            if (filteringOptions.isNotEmpty()) {
-                ExposedDropdownMenu(
-                    expanded = expanded,
-                    onDismissRequest = {
-                        expanded = false
-                    }
-                ) {
-                    filteringOptions.forEach { selectionOption ->
-                        DropdownMenuItem(
-                            onClick = {
-                                selectedOptionText = selectionOption
-                                expanded = false
-                            },
-                            modifier = Modifier,
-                            text = {Text(text = selectionOption)}
-                        )
-                    }
-                }
-            }
-        }
-    }
-
-    @Composable
-    fun Sample2() {
-        var expanded by remember { mutableStateOf(false) }
-        Column() {
-            Text(text = "Before")
-
-            IconButton(onClick = { expanded = true }) {
-                Icon(
-                    imageVector = Icons.Default.MoreVert,
-                    contentDescription = "klqklklqs"
-                )
-            }
-            DropdownMenu(
-                expanded = expanded,
-                onDismissRequest = { expanded = false },
-            ) {
-                DropdownMenuItem(
-                    onClick = {
-                        //navigator.navigate(Root.Draft.Compose(it.draftId))
-                    },
-                    text = { Text(text = "edit") }
-                )
-                DropdownMenuItem(
-                    onClick = {
-                        //      viewModel.delete(it)
-                    },
-                    text = {
-                        Text(
-                            text = "remove",
-                            color = Color.Red,
-                        )
-                    }
-                )
-            }
-        }
-        Column() {
-            Text(text = "After")
-            Text(text = "After1")
-            Text(text = "After2")
-            Text(text = "After3")
-            Text(text = "After4")
-        }
-
-    }
-
-    @OptIn(ExperimentalMaterial3Api::class)
-    @Composable
-    fun docs() {
-        val options = listOf("Option 1", "comment", "Afghanistan", "Albania", "Algeria", "Andorra", "Egypt")
-        var expanded by remember { mutableStateOf(false) }
-        var selectedOptionText by remember { mutableStateOf("") }
-        ExposedDropdownMenuBox(
-            expanded = expanded,
-            onExpandedChange = { expanded = !expanded },
-        ) {
-            TextField(
-                // The `menuAnchor` modifier must be passed to the text field for correctness.
-                modifier = Modifier.menuAnchor(),
-                value = selectedOptionText,
-                onValueChange = { selectedOptionText = it },
-                label = { Text("Label") },
-                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
-                colors = ExposedDropdownMenuDefaults.textFieldColors(
-                    //focusedContainerColor = Color.White,
-                    //unfocusedContainerColor = Color.White
-                ),
-            )
-            // filter options based on text field value
-            val filteringOptions = options.filter { it.contains(selectedOptionText, ignoreCase = true) }
-            if (filteringOptions.isNotEmpty()) {
-                DropdownMenu(
-                    modifier = Modifier
-                        .background(Color.White)
-                        .exposedDropdownSize(true)
-                    ,
-                    properties = PopupProperties(focusable = false),
-                    expanded = expanded,
-                    onDismissRequest = { expanded = false },
-                ) {
-                    filteringOptions.forEach { selectionOption ->
-                        DropdownMenuItem(
-                            text = { Text(selectionOption) },
-                            onClick = {
-                                selectedOptionText = selectionOption
-                                expanded = false
-                            },
-                            contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
-                        )
-                    }
-                }
-            }
-        }
-    }
-
-    private fun addRandomTodo() {
-
-
-        for (i in 0..20) {
-            this.lifecycle.coroutineScope.launch(Dispatchers.IO) {
-                try {
-                    Log.i("DataStore", "TODO_START $i")
-                    val todo = Todo.builder().name("The name").description("The description").build()
-                    Amplify.DataStore.save(todo)
-                    Amplify.DataStore.delete(todo)
-                    Log.i("DataStore", "TODO_END $i")
-                } catch (error: DataStoreException) {
-                    Log.e("DataStoreException", "Saved new question failed.", error)
-                }
-            }
+    private fun refreshdatastore() {
+        this.lifecycle.coroutineScope.launch(Dispatchers.IO) {
+            Amplify.DataStore.stop()
+            Amplify.DataStore.start()
         }
     }
 
@@ -292,10 +80,13 @@ class MainActivity : ComponentActivity() {
         try {
             Log.i("MyAmplifyApp", "Initialized Amplify")
 
-            Amplify.addPlugin(AWSDataStorePlugin())
-            Amplify.addPlugin(AWSApiPlugin())
-            Amplify.addPlugin(AWSDataStorePlugin())
+            Amplify.addPlugin(AWSDataStorePlugin.builder().dataStoreConfiguration(
+                DataStoreConfiguration.builder()
+                    .syncExpression(User::class.java) { User.LAST_NAME.eq("Doe") }
+                    .build())
+                .build())
 
+            Amplify.addPlugin(AWSApiPlugin())
             Amplify.configure(mainActivity)
 
             Log.i("MyAmplifyApp", "Initialized Amplify")
@@ -311,6 +102,5 @@ class MainActivity : ComponentActivity() {
             Greeting("Android")
         }
     }
-
 
 }
